e66d243d34df1fb1fae761958dac27e561009486
Jordan FERLICOT <j.ferlicot@highconnexion.com>
2018-11-20 15:16:30 +0100

    Correctif bug chargement de vue dans fonction

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ function display_view($view_name, $data = array(), $return = false) {
     extract($data);
 
     ob_start();
-    require_once '../views/' . $view_name . '.php';
+    require_once __DIR__ . '/../views/' . $view_name . '.php';
     $content = ob_get_contents();
     ob_end_flush();
 
