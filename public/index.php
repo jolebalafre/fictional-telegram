719a1c43e2fa6dbd4f8ed3d0ead9b576d16164ab
Jordan FERLICOT <j.ferlicot@highconnexion.com>
2018-11-20 15:18:16 +0100

    Correctif bug doulbe affichage vue dans fonction

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ function display_view($view_name, $data = array(), $return = false) {
     ob_start();
     require_once __DIR__ . '/../views/' . $view_name . '.php';
     $content = ob_get_contents();
-    ob_end_flush();
+    ob_end_clean();
 
     if ($return) {
         return $content;
